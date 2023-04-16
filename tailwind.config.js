1310d29992a9dc0bc3fdfeb068c42e8eebe62d48
Aliphon Fen <redshiftkeying@gmail.com>
2023-04-16 20:00:18 +0800

    feat: tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './src/**/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
